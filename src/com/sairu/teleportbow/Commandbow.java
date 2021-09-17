28d66bf8bba785ec09b023b0937d5208f3789be6
SAIRU <81401104+Sairu@users.noreply.github.com>
2021-09-17 01:23:39 -0300

    Update

--- src/com/sairu/teleportbow/Commandbow.java
+++ src/com/sairu/teleportbow/Commandbow.java
@@ -1,17 +1,21 @@
 package com.sairu.teleportbow;
 
 import Item.itemManager;
+import org.bukkit.Material;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class Commandbow implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command command, String lable, String[] args) {
         if (sender instanceof Player) {
             Player player = (Player)sender;
-            player.getInventory().addItem(itemManager.TeleportBow);
+            ItemStack arrow = new ItemStack(Material.ARROW,1);
+            arrow.setAmount(32);
+            player.getInventory().addItem(itemManager.TeleportBow,arrow);
             player.sendMessage("§dYou recive a Teleport Bow");
         }
         return true;
